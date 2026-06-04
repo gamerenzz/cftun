faa4f2f19ced255ae18760c52ce9ce6c8cc14901
gamerenzz <36974615+gamerenzz@users.noreply.github.com>
2026-06-05 05:43:42 +0800

    Update main.go

--- main.go
+++ main.go
@@ -3,15 +3,17 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"os"
+	"os/signal"
+	"path/filepath"
+	"runtime"
+	"syscall"
+	"time"
+
 	"github.com/fmnx/cftun/client"
 	"github.com/fmnx/cftun/log"
 	"github.com/fmnx/cftun/server"
 	"github.com/spf13/pflag"
-	"os"
-	"os/signal"
-	"path/filepath"
-	"syscall"
-	"time"
 )
 
 type RawConfig struct {
@@ -36,7 +38,6 @@ func parseConfig(configFile string) (*RawConfig, error) {
 		return nil, err
 	}
 	return rawCfg, nil
-
 }
 
 var (
@@ -46,9 +47,9 @@ var (
 	proxy4             bool
 	proxy6             bool
 	port               int
-	Version            = "unknown"
+	Version            = "2.3.0"
 	BuildDate          = "unknown"
-	BuildType          = "DEV"
+	BuildType          = "RELEASE"
 	CloudflaredVersion = "2025.4.1"
 	showVersion        bool
 	quickData          = &server.QuickData{}
@@ -56,7 +57,7 @@ var (
 )
 
 func init() {
-	pflag.StringVarP(&configFile, "config", "c", "./config.json", "")
+	pflag.StringVarP(&configFile, "config", "c", "", "")
 	pflag.StringVarP(&token, "token", "t", "", "")
 	pflag.BoolVarP(&isQuick, "quick", "q", false, "")
 	pflag.BoolVarP(&proxy4, "proxy4", "4", false, "")
@@ -66,91 +67,66 @@ func init() {
 
 	pflag.Usage = func() {
 		fmt.Println("Usage:")
-		fmt.Printf("  -c,--config\tSpecify the path to the config file.(default: \"./config.json\")\n")
-		fmt.Printf("  -t,--token\tWhen a token is provided, the configuration file will be ignored and the program will run in server mode only.\n")
-		fmt.Printf("  -q,--quick\tTemporary server, no Cloudflare account required, based on try.cloudflare.com.\n")
-		fmt.Printf("  -4,--proxy4\tUse the WARP proxy for IPv4 traffic; Ignored when using a configuration file.\n")
-		fmt.Printf("  -6,--proxy6\tUse the WARP proxy for IPv4 traffic; Ignored when using a configuration file.\n")
-		fmt.Printf("  -p,--port\tSet the local port for WARP; Ignored when using a configuration file.\n")
-		fmt.Printf("  -v,--version\tDisplay the current binary file version.\n")
+		fmt.Printf("  -c,--config\tSpecify the path to the config file.\n")
+		fmt.Printf("  -t,--token\tRun in server mode only using the provided token.\n")
+		fmt.Printf("  -v,--version\tDisplay the version.\n")
 	}
 	pflag.Parse()
 }
 
-func main() {
+func runCoreEngine() {
 	bInfo := server.GetBuildInfo(BuildType, CloudflaredVersion)
-	if showVersion {
-		printVersion(bInfo)
+	rawConfig, err := parseConfig(configFile)
+	if err != nil {
+		log.Errorln("Failed to parse config file: %s", err.Error())
 		return
 	}
-	if token != "" || isQuick { // command line.
-		var warp *server.Warp
-		if proxy4 || proxy6 {
-			warp = &server.Warp{
-				Auto:   true,
-				Port:   uint16(port),
-				Proxy4: proxy4,
-				Proxy6: proxy6,
+
+	c := rawConfig.Client
+	if c != nil {
+		if c.Tun != nil && c.Tun.Enable {
+			tunName = c.Tun.Name
+			if tunName == "" {
+				tunName = "cftun0"
+				c.Tun.Name = tunName
 			}
 		}
-		if isQuick {
-			token = "quick"
-		} else if token == "quick" {
+		c.Run()
+	}
+
+	time.Sleep(100 * time.Millisecond)
+
+	s := rawConfig.Server
+	if s != nil {
+		if s.Token == "quick" {
 			isQuick = true
 		}
-		srv := &server.Config{
-			Token:  token,
-			HaConn: 4,
-			Warp:   warp,
-		}
-		go srv.Run(bInfo, quickData)
-	} else {
-		rawConfig, err := parseConfig(configFile)
-		if err != nil {
-			log.Fatalln("Failed to parse config file: ", err.Error())
-		}
-
-		c := rawConfig.Client
-		if c != nil {
-			if c.Tun != nil && c.Tun.Enable {
-				tunName = c.Tun.Name
-				if tunName == "" {
-					tunName = "cftun0"
-					c.Tun.Name = tunName
-				}
-			}
-			c.Run()
-		}
-
-		time.Sleep(100 * time.Millisecond)
-
-		s := rawConfig.Server
-		if s != nil {
-			if s.Token == "quick" {
-				isQuick = true
-			}
-			go s.Run(bInfo, quickData)
-		}
-
+		go s.Run(bInfo, quickData)
 	}
+}
+
+func main() {
+	// Windows 平台，如果没有传入 CLI 命令，则自动切换至 Native 专属图形界面
+	if runtime.GOOS == "windows" && configFile == "" && token == "" && !isQuick {
+		log.Infoln("[System] Double-click detected. Redirecting to Graphical Panel...")
+		client.StartWindowsGUI(runCoreEngine)
+		return
+	}
+
+	if showVersion {
+		bInfo := server.GetBuildInfo(BuildType, CloudflaredVersion)
+		fmt.Printf("CFTunVersion: %s\nBuildDate: %s\n", Version, BuildDate)
+		return
+	}
+
+	runCoreEngine()
 
 	sigCh := make(chan os.Signal, 1)
 	signal.Notify(sigCh, syscall.SIGINT, syscall.SIGTERM)
-	for {
-		select {
-		case <-sigCh:
-			if isQuick {
-				quickData.Save()
-			}
-			if tunName != "" {
-				client.DeleteTunDevice(tunName)
-			}
-			return
-		}
+	<-sigCh
+
+	if tunName != "" {
+		client.DeleteTunDevice(tunName)
 	}
 }
 
-func printVersion(buildInfo *server.BuildInfo) {
-	fmt.Printf("GoOS: %s\nGoArch: %s\nGoVersion: %s\nBuildType: %s\nCftunVersion: %s\nBuildDate: %s\n",
-		buildInfo.GoOS, buildInfo.GoArch, buildInfo.GoVersion, buildInfo.BuildType, Version, BuildDate)
-}
